1325bcb589cfb1959e1c10bf80a9f0a3f554dab8
Claudio Vasquez <clauvzq150@gmail.com>
2025-06-30 23:39:19 -0300

    Project created

---
+++ src/main/java/org/khenas/teams/Teams.java
@@ -0,0 +1,17 @@
+package org.khenas.teams;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Teams extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
