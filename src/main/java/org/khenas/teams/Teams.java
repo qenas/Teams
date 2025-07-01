67a12d9a77a3dc184391e47330ce39ee661b4ce2
Claudio Vasquez <clauvzq150@gmail.com>
2025-06-30 23:52:06 -0300

    First command implemented: Teams

--- src/main/java/org/khenas/teams/Teams.java
+++ src/main/java/org/khenas/teams/Teams.java
@@ -1,13 +1,13 @@
 package org.khenas.teams;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.khenas.teams.commands.Create;
 
 public final class Teams extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getCommand("create").setExecutor(new Create());
     }
 
     @Override
---
+++ src/main/java/org/khenas/teams/commands/Create.java
@@ -0,0 +1,25 @@
+package org.khenas.teams.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class Create implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String @NotNull [] strings) {
+        if(!(commandSender instanceof Player)){
+            commandSender.sendMessage("Command only available for real players.");
+            return true;
+        }
+        Player player = (Player) commandSender;
+        ArrayList<Player> teamList = new ArrayList<Player>();
+        teamList.add(player);
+        commandSender.sendMessage("Team created. The leader are -" + player.getName());
+        return false;
+    }
+}
